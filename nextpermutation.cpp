37ed220383df8b7258861dfb541986bc424ff26d
pandeyup51 <84857891+pandeyup51@users.noreply.github.com>
2023-06-05 23:13:59 +0530

    Add files via upload

---
+++ nextpermutation.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &arr, int n)
+{
+    //  Write your code here.
+      next_permutation(arr.begin(), arr.end()) ;
+           
+     
+      return arr;
+}
